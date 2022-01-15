56ce85637559a7504af529991ed36cdb16c0dbf8
Yaroslam <47461457+Yaroslam@users.noreply.github.com>
2022-01-15 22:22:28 +0300

    Delete src directory

--- src/api/router.js
+++
@@ -1,4 +0,0 @@
-export const ARTICLES = 'http://demo-api.vsdev.space/api/articles'
-export const ARTICLE_BY_ID = (id) => `http://demo-api.vsdev.space/api/articles/${id}`
-export const COMMENTS_TO_ARTICLE = (id) => `http://demo-api.vsdev.space/api/articles/${id}/comments`
-
