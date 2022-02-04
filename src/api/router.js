23d0f5a9cb1ab74071478d0d1d3ff333889b1a55
Yaroslam <47461457+Yaroslam@users.noreply.github.com>
2022-02-04 17:20:49 +0300

    Update router.js

--- src/api/router.js
+++ src/api/router.js
@@ -1,4 +1,4 @@
 export const ARTICLES = 'https://demo-api.vsdev.space/api/articles'
-export const ARTICLE_BY_ID = (id) => `http://demo-api.vsdev.space/api/articles/${id}`
-export const COMMENTS_TO_ARTICLE = (id) => `http://demo-api.vsdev.space/api/articles/${id}/comments`
+export const ARTICLE_BY_ID = (id) => `https://demo-api.vsdev.space/api/articles/${id}`
+export const COMMENTS_TO_ARTICLE = (id) => `https://demo-api.vsdev.space/api/articles/${id}/comments`
 
